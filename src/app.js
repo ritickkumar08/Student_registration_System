553697fc00179d46373b051d2599838a8a51f3d3
ritickkumar <ritickkumar08@gmail.com>
2025-10-19 12:35:04 +0530

    adding after adding my js file

--- src/app.js
+++ src/app.js
@@ -10,5 +10,71 @@ let students = JSON.parse(localStorage.getItem("student") || []) // here we use 
 renderStudents();
 
 //form data handling
-form.addEventListener()
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();//it prevents to submit the default values of the form.
+
+
+    //selecting the the form elements and getting accessing the values entered 
+    //to store the input value in a variable for furthur use.
+    const name = document.querySelector("#name").value.trim()
+    const studentID = document.querySelector("#studentID").value.trim()
+    const email = document.querySelector("#Email").value.trim()
+    const contact = document.querySelector("#number").value.trim()
+
+    //checking for the validations of the input values of the form element.
+    if(!/^[A-Za-z\s]+$/.test(name)) return alert(`name can only contain letters`)
+    if(!/^\d+$/.test(studentID)) return alert(`Student id must be numeric`)
+    if(!email.includes("@")) return alert(`enter a valid email`)
+    if(!/^\d{10}$/.test(contact)) return alert(`contact must be of 10 digits`)
+ 
+    const newStudent = [name, studentID, email,contact]
+    students.push(newStudent);
+    saveToLocalStorage(); //calling a function to save the entered data to the localstorage.
+    renderStudents();
+    form.reset()
+})
+
+function renderStudents() {
+    studentBody.innerHTML = ""
+    students.forEach((stu,index) => {
+        const row = document.createElement("tr")
+        row.innerHTML = `
+        <td class="border border-gray-300 px-4 py-2">${index + 1}</td>
+        <td class="border border-gray-300 px-4 py-2">${stu.name}</td>
+        <td class="border border-gray-300 px-4 py-2">${stu.studentID}</td>
+        <td class="border border-gray-300 px-4 py-2">${stu.email}</td>
+        <td class="border border-gray-300 px-4 py-2">${stu.contact}</td>
+        <td>
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-white px-2 py-1 rounded" onclick="editStudent(${index})">Edit</button>
+        <button class="bg-red-600 hover:bg-red-700 text-white px-2 py-1 rounded" onclick="deleteStudent(${index})">Delete</button>
+        </td>`
+        studentBody.appendChild(row)
+    });
+}
+
+
+//adding edit functionality
+function editStudent(index){
+    const stu = students[index];
+    document.querySelector("#name").value = stu.name
+    document.querySelector("#studentID").value = stu.studentID
+    document.querySelector("#Email").value = stu.email
+    document.querySelector("#number").value = stu.contact
+
+    students.splice(index,1); //removing the old recoed temprorily
+    saveToLocalStorage()
+    renderStudents()
+}
+
+function deleteStudent(index) {
+    if(confirm('you want to delete this record')){
+        students.splice(index,1);
+        saveToLocalStorage()
+        renderStudents();
+    }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("students",JSON.stringify(students))
+}
 
